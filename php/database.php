b957173d3e13c736f73c3ded60616cc22413a921
feisalmombo <feisalmombo29@gmail.com>
2023-07-26 12:18:21 +0300

    WIP

--- php/database.php
+++ php/database.php
@@ -1,12 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Content-Type: application/json; charset=UTF-8");
 
- $db_username = 'root';
- $db_password = '';
- $db_name = 'register-login-system';
- $db_host = 'localhost';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'register-login-system';
+$db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
 if ($mysqli->connect_error) {
