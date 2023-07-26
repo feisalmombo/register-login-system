d70e3bd6add654cc3b31af56ca6e875d8b10a9a3
feisalmombo <feisalmombo29@gmail.com>
2023-07-26 16:35:50 +0300

    WIP

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class DataserviceService {
+  redirectUrl: string;
 
-  constructor() { }
+  baseUrl:string = "http://localhost/uat/api";
+@Output() getLoggedInName: EventEmitter<any> = new EventEmitter();
+  constructor(private httpClient : HttpClient) { }
+  public userlogin(username, password) {
+    return this.httpClient.post<any>(this.baseUrl + '/login.php', { username, password })
+        .pipe(map(Usermodule => {
+            this.setToken(Usermodule[0].name);
+            this.getLoggedInName.emit(true);
+            return Usermodule;
+        }));
+}
+public userregistration(name,email,pwd) {
+  return this.httpClient.post<any>(this.baseUrl + '/registration.php', { name,email, pwd })
+      .pipe(map(Usermodule => {
+          return Usermodule;
+      }));
+}
+//token
+setToken(token: string) {
+  localStorage.setItem('token', token);
+}
+
+getToken() {
+  return localStorage.getItem('token');
+}
+
+deleteToken() {
+  localStorage.removeItem('token');
+}
+
+isLoggedIn() {
+  const usertoken = this.getToken();
+  if (usertoken != null) {
+    return true
+  }
+  return false;
 }
