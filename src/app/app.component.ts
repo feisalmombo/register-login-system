e7ce1a1ff0d5da1f53c86b5e73b7d1e1e42a25fc
feisalmombo <feisalmombo29@gmail.com>
2023-07-26 17:33:56 +0300

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'register-login-system';
+  loginbtn:boolean;
+  logoutbtn:boolean;
+  constructor(private apiService: ApiService) {
+    apiService.getLoggedInName.subscribe(name => this.changeName(name));
+    if(this.apiService.isLoggedIn())
+    {
+      console.log("loggedin");
+      this.loginbtn=false;
+      this.logoutbtn=true
+    }
+    else{
+     this.loginbtn=true;
+     this.logoutbtn=false
+    }
+
+
 }
+private changeName(name: boolean): void {
+  this.logoutbtn = name;
+  this.loginbtn = !name;
+}
+logout()
+{
+  this.apiService.deleteToken();
+  window.location.href = window.location.href;
+}
+}
+
