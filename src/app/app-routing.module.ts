561d457200f16284e45a269c9ffece54f2b90621
feisalmombo <feisalmombo29@gmail.com>
2023-07-31 12:58:58 +0300

    edit routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthguardGuard } from './authguard.guard';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent,canActivate: [AuthguardGuard] }
+  { path: 'dashboard', component: DashboardComponent,canActivate: [AuthguardGuard] },
+  {path: '**', component: PageNotFoundComponent}
 
 ]
 
